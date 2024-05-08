44a71752a3b9958e457810d52ef81d6236ff635d
Jer-Tran <jeremy.tran83@gmail.com>
2024-05-08 21:32:21 +1000

    Job instances created from data of local file

--- src/jobs.js
+++ src/jobs.js
@@ -1,3 +1,7 @@
+// import jobs from './jobs.json' assert { type: "json" };
+
+var _jobs
+
 class Mitigation {
     constructor(name, icon, phys, magic) {
         this.name = name;
@@ -29,7 +33,7 @@ class Job {
 
     toString() {
         var m = ""
-        for (i in this.mits) {
+        for (let i in this.mits) {
             m = m + this.mits[i].toString()
         }
         return this.name + " " + this.role + " " + this.icon + " mits: [" + m + "]"
@@ -46,29 +50,39 @@ class Job {
 
     getMitiIcons() {
         let x = document.createElement("div")
-        for (i in this.mits) {
+        for (let i in this.mits) {
             x.appendChild(this.mits[i].getIcon())
         }
         return x
     }
 }
 
-function createJob(name) {
-    // Read job data from a file, then create job instance using data
+function createJob(party, name) {
+    for (let i in _jobs) {
+        if (_jobs[i].name == name) {
+            console.log(_jobs[i])
+            let j = _jobs[i]
+            var x = new Job(j.name, j.role, j.icon, [new Mitigation("reprisal", "/bar.ico", 0.5, 0.5)])
+            party.push(x)
+            return
+        }
+    }
+
     // The file will contain job name, its role, link to their job icon, and their personal job mits
-    return new Job(name, "bar", "/foo.ico", [new Mitigation("reprisal", "/bar.ico", 0.5, 0.5)])
+    // return new Job(name, "bar", "/foo.ico", [new Mitigation("reprisal", "/bar.ico", 0.5, 0.5)])
+    console.log("Unable to find job named: " + name)
 }
 
 function displayParty(party) {
-    plist = document.getElementById("party")
+    let plist = document.getElementById("party")
     plist.innerHTML = "";
-    for (i in party) {
+    for (let i in party) {
         // Create a div/something for each member
-        member = document.createElement("div")
+        let member = document.createElement("div")
         member.innerText = party[i].toString()
         plist.appendChild(member)
         plist.appendChild(party[i].getIcon())
-        plist.appendChild(party[i].getMitiIcons())
+        // plist.appendChild(party[i].getMitiIcons())
         
         console.log(party[i])
         // Fill the container
@@ -76,12 +90,32 @@ function displayParty(party) {
     }
 }
 
+function doError() {
+    // console.log("send help")
+}
+
 function test() {
     party = []
-    party.push(createJob('paladin'))
-    party.push(createJob('rogue'))
+    console.log(party)
+    createJob(party, 'paladin')
+    console.log(party)
+    createJob(party, 'rogue')
+    console.log(party)
 
     displayParty(party)
 }
 
-test()
+
+function start(jobs) {
+    _jobs = jobs // Changing to a dictionary is a good idea down the line
+    console.log(_jobs)
+    // const jobs = require('./jobs.json')
+    // console.log(jobs)
+    // Considering making the .then of this function be start(data)
+    
+    test()
+}
+
+fetch("./data/jobs.json").then(res => {return res.json();}).then(jobs => 
+    start(jobs)
+).catch(doError())
